96dd91c8024e580ceff141287f04d4d4c6558eb3
GitPit <rifflifter@gmail.com>
2022-06-17 13:54:17 -0400

    export default function App() {

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+export default function App() {
 
-function App() {
   return (
     <div className="App">
       
@@ -9,4 +9,5 @@ function App() {
   );
 }
 
-export default App;
+
+
