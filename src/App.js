4aef43622e72a05e158f43f5929baa58e2861d55
GitPit <rifflifter@gmail.com>
2022-06-17 13:55:30 -0400

    const for lat and long

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 export default function App() {
 
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
   return (
     <div className="App">
       
