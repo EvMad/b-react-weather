4959e6a0d305a707ece9ab3945cac60538e680a4
GitPit <rifflifter@gmail.com>
2022-07-01 15:25:27 -0400

    removed dead imports from app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,4 +12,3 @@ root.render(
 );
 
 
-reportWebVitals();
